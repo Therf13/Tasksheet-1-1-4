e8e22bd173756767bd1f2bedd09141621a249273
Therf13 <157944898+Therf13@users.noreply.github.com>
2024-08-16 12:15:01 +0000

    Finish

--- EvenOdd.java
+++ EvenOdd.java
@@ -1,1 +1,14 @@
-
+class Controlflow
+{
+    public static void main(String[] args)
+    {
+        int check_number = 10;
+        for (int i=1; i <=check_number; i++)
+            if (i%2==0) {
+                System.out.println(i + " is Even Number");
+            }
+            else {
+                System.out.println(i + " is Odd Number");
+            }
+    }
+}
